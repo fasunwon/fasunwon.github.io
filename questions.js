09bb0e9d1fd89577cd6405fae272e72f0147260d
fasunwon <71341030+fasunwon@users.noreply.github.com>
2021-01-30 21:58:02 -0600

    initial commit

---
+++ questions.js
@@ -0,0 +1,111 @@
+let questions = [{
+        number: 1,
+        question: "The Gospel of Mark begins with?",
+        answer: "Jesus's baptism by John",
+        options: [
+            "John the Baptist's Birth",
+            "Jesus's Birth",
+            "The angel Gabriel appearing to Mary",
+            "Jesus's baptism by John"
+        ]
+    },
+    {
+        number: 2,
+        question: "John the forerunner of Jesus, Proclaims forgiveness of sins and performs:",
+        answer: "Baptisms",
+        options: [
+            "Circumcisions",
+            "Baptisms",
+            "Weddings",
+            "Miracles"
+        ]
+    },
+    {
+        number: 3,
+        question: "When Jesus is baptized, the spirit of God descends on him in the shape of:",
+        answer: "A Dove",
+        options: [
+            "A Dove",
+            "Flames",
+            "Angels",
+            "A sunbeam"
+        ]
+    },
+    {
+        number: 4,
+        question: "All of the following figures recognize Jesus in Mark except:",
+        answer: "John the Baptist",
+        options: [
+            "The Demons",
+            "John the Baptist",
+            "The Gentile soldier at the cross",
+            "The reader of the Gospel"
+        ]
+    },
+    {
+        number: 5,
+        question: "The phrase 'Jesus Passion' refers to:",
+        answer: "Jesus' Suffering",
+        options: [
+            "Jesus's Birth",
+            "Jesus' violent actions in the Temple",
+            "Jesus' Love for Humanity",
+            "Jesus' Suffering"
+        ]
+    },
+    {
+        number: 6,
+        question: "According to Mark, the messiah must:",
+        answer: "Suffer and Die",
+        options: [
+            "Suffer and Die",
+            "Preach and Baptize",
+            "Become the high Priest",
+            "Take control of Israel"
+        ]
+    },
+    {
+        number: 7,
+        question: "In the first-century apocalyptic Judaism, the term 'Son of Man' referred to:",
+        answer: "A cosmic figure",
+        options: [
+            "God's Son",
+            "A cosmic figure",
+            "The King",
+            "The high Priest"
+        ]
+    },
+    {
+        number: 8,
+        question: "At Jesus' trial in Mark, the Sanhedrin charges him with:",
+        answer: "Blasphemy",
+        options: [
+            "Theft",
+            "Blasphemy",
+            "Being empowered by Satan",
+            "Not observing Sabbath laws"
+        ]
+    },
+    {
+        number: 9,
+        question: "In Mark, the Temple curtain rips:",
+        answer: "At Jesus' Death",
+        options: [
+            "At Jesus' Birth",
+            "At Jesus' Trial",
+            "At Jesus' Death",
+            "At Jesus' Resurrection"
+        ]
+    },
+    {
+        number: 10,
+        question: "The Gospel of Mark was probably written around:",
+        answer: "65-70 C.E.",
+        options: [
+            "35-40 C.E",
+            "45-50 C.E",
+            "55-60 C.E",
+            "65-70 C.E."
+        ]
+    }
+];
